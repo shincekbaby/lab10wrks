7dc5530fefd155d5297c9bbf92ca99d4a4c36aa3
shincekbaby <42138520+shincekbaby@users.noreply.github.com>
2018-11-17 14:17:57 +0530

    Add files via upload

---
+++ lab10_q5.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+class ZooAnimal  
+   {
+      char *name;
+      int cageNumber;
+      int weightDate;
+      int weight;
+    public:
+      void Destroy (); // destroy function
+      char* reptName ();
+      int daysSinceLastWeighed (int today);
+      void create (char * p,int q,int r,int s);
+   };
+void ZooAnimal::create(char *p,int c,int wd ,int w)
+{
+  name=p;
+  cageNumber=c;
+  weightDate=wd;
+  weight=w;
+}
+ 
+void ZooAnimal::Destroy ()
+   {    
+    delete [] name;
+   }
+
+   //member function to return the animal's name
+   char* ZooAnimal::reptName ()
+   {
+    return name;
+   }
+   
+   //member function to return the number of days
+   //since the animal was last weighed
+  int ZooAnimal::daysSinceLastWeighed (int today)
+   {
+    int startday, thisday;
+    thisday = today/100*30 + today - today/100*100;
+    startday = weightDate/100*30 + weightDate - weightDate/100*100;
+    if (thisday < startday) 
+       { thisday += 360;
+    return (thisday-startday);}
+
+    if (today < weightDate) 
+       { today += 360;
+      return (today-weightDate);}
+   }
+
+int main()
+{
+    ZooAnimal bozo;
+    bozo.create ("Bozo", 408, 1027, 400);
+
+    cout << "This animal's name is " << bozo.reptName() << endl;
+
+    //bozo.Destroy ();function not called due to lack of definition
+    return 0;
+}
---
+++ lab10_q6.1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+enum scale {ounces, kilograms};
+
+class ZooAnimal  
+{
+private:
+    char *name;
+    int cageNumber;
+    int weightDate;
+    int weight;
+public:
+    void Create (char*, int, int, int); // create function
+    void Destroy (); // destroy function 
+    void changeWeight (int pounds);
+    void changeWeightDate (int today);
+    char* reptName ();
+    int reptWeight ();
+    void reptWeight (scale which);
+    inline int reptWeightDate ();
+    int daysSinceLastWeighed (int today);
+};
+//Member function
+void ZooAnimal::Create (char* a, int b, int c, int d)
+{
+    name=a;
+    cageNumber=b;
+    weightDate=c;
+    weight=d;
+}
+inline int ZooAnimal::reptWeightDate ()
+{
+    return weightDate;
+}
+
+
+int main()
+{
+    ZooAnimal bozo;
+    bozo.Create ("Bozo", 408, 1027, 400);
+
+    cout << "The day the animal was last weighed " << bozo.reptWeightDate() << endl;
+    return 0;
+}
+
+
+
